35366bfdac1c0a11ad0617b6f86e7854e0ce041a
colajam93 <fs.fake.g1@gmail.com>
2016-07-18 14:45:51 +0900

    Add song model

--- song/models.py
+++ song/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+_MAX_LENGTH = 100
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=_MAX_LENGTH)
+    artist = models.CharField(max_length=_MAX_LENGTH)
+    album = models.CharField(max_length=_MAX_LENGTH)
+    album_artist = models.CharField(max_length=_MAX_LENGTH)
+    genre = models.CharField(max_length=_MAX_LENGTH)
+    track_number = models.IntegerField()
+    track_count = models.IntegerField()
+    year = models.IntegerField()
