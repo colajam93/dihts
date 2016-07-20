776de94c4c7b19b7178aba4f3d52c373d8cc16e4
colajam93 <fs.fake.g1@gmail.com>
2016-07-21 00:50:11 +0900

    View list OK

--- song/views.py
+++ song/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from song.models import Song
+from song.models import Song, Album
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-SEARCH_FOR, SEARCH_TYPE, SONGS, = ('search_for', 'search_type', 'songs')
+SEARCH_FOR, SEARCH_TYPE, KEYWORD, RESULTS = ('search_for', 'search_type', 'keyword', 'results')
 ALBUM, SONG = ('album', 'song')
 CONTAINS, EXACT, STARTSWITH, ENDSWITH = ('contains', 'exact', 'startswith', 'endswith')
 
@@ -16,9 +16,19 @@ def search(request):
     params[SEARCH_TYPE] = search_type
 
     try:
-        keyword = request.GET['keyword']
-        params[SONGS] = Song.objects.filter(title__contains=keyword)
+        keyword = request.GET[KEYWORD]
+        params[KEYWORD] = keyword
+        if search_for == ALBUM:
+            filter_param = {'value__{}'.format(search_type): keyword}
+            params[RESULTS] = map(lambda x: {'column1': x.value},
+                                  Album.objects.filter(**filter_param).order_by('value'))
+        elif search_for == SONG:
+            filter_param = {'title__{}'.format(search_type): keyword}
+            params[RESULTS] = map(lambda x: {'column1': x.title, 'column2': x.artist, 'column3': x.album},
+                                  Song.objects.filter(**filter_param).order_by('album__value', 'artist__value',
+                                                                               'title'))
     except KeyError:
-        params[SONGS] = []
+        params[RESULTS] = []
+        params[KEYWORD] = ''
 
     return render(request, 'search.html', params)
