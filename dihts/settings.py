e8d085f308efd8accfae301800d6fe6dba87c266
colajam93 <fs.fake.g1@gmail.com>
2016-07-23 13:35:17 +0900

    Search result ok

--- dihts/settings.py
+++ dihts/settings.py
@@ -118,4 +118,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'external', 'static'),
+    os.path.join(BASE_DIR, 'common', 'static'),
 )
--- song/views.py
+++ song/views.py
@@ -2,33 +2,76 @@ from django.shortcuts import render
 from song.models import Song, Album
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-SEARCH_FOR, SEARCH_TYPE, KEYWORD, RESULTS = ('search_for', 'search_type', 'keyword', 'results')
+FOR, IN, KEYWORD, MATCH, RESULTS = ('for', 'in', 'keyword', 'match', 'results')
 ALBUM, SONG = ('album', 'song')
-CONTAINS, EXACT, STARTSWITH, ENDSWITH = ('contains', 'exact', 'startswith', 'endswith')
+TITLE, ARTIST, GENRE, ALBUM_ARTIST, YEAR = ('title', 'artist', 'genre', 'album_artist', 'year')
+CONTAINS, EXACT, FORWARD, BACKWARD = ('contains', 'exact', 'forward', 'backward')
+
+
+def _match_suffix(match):
+    if match == CONTAINS:
+        return '{}__contains'
+    elif match == EXACT:
+        return '{}__exact'
+    elif match == FORWARD:
+        return '{}__startswith'
+    elif match == BACKWARD:
+        return '{}__endswith'
+
+
+def _search_album(**params):
+    search_in = params[IN]
+    match_suffix = _match_suffix(params[MATCH])
+    q = ''
+    if search_in == TITLE:
+        q = 'album'
+    elif search_in == ARTIST:
+        q = 'album_artist__value'
+    elif search_in == GENRE:
+        q = 'album_artist__genre__value'
+    elif search_in == ALBUM_ARTIST:
+        q = 'album_artist__value'
+    elif search_in == YEAR:
+        q = 'year__value'
+    query = {match_suffix.format(q): params[KEYWORD]}
+    result = Album.objects.filter(**query).order_by('album', 'album_artist')
+    return map(lambda x: {'title': x.album, 'album_artist': x.album_artist, 'artist': '', 'album': ''}, result)
+
+
+def _search_song(**params):
+    search_in = params[IN]
+    match_suffix = _match_suffix(params[MATCH])
+    q = ''
+    if search_in == TITLE:
+        q = 'title'
+    elif search_in == ARTIST:
+        q = 'artist__artist'
+    elif search_in == GENRE:
+        q = 'album__album_artist__genre__value'
+    elif search_in == ALBUM_ARTIST:
+        q = 'album__album_artist__value'
+    elif search_in == YEAR:
+        q = 'album__year__value'
+    query = {match_suffix.format(q): params[KEYWORD]}
+    result = Song.objects.filter(**query).order_by('album__album_artist', 'album', 'title', 'artist')
+    return map(lambda x: {'title': x.title, 'album_artist': x.album.album_artist, 'artist': x.artist, 'album': x.album},
+               result)
 
 
 @ensure_csrf_cookie
 def search(request):
-    params = {}
-    search_for = request.GET.get(SEARCH_FOR, ALBUM)
-    params[SEARCH_FOR] = search_for
-    search_type = request.GET.get(SEARCH_TYPE, CONTAINS)
-    params[SEARCH_TYPE] = search_type
+    search_for = request.GET.get(FOR, ALBUM)
+    search_in = request.GET.get(IN, TITLE)
+    search_keyword = request.GET.get(KEYWORD, '')
+    search_match = request.GET.get(MATCH, CONTAINS)
+    params = {FOR: search_for, IN: search_in, KEYWORD: search_keyword, MATCH: search_match}
 
-    try:
-        keyword = request.GET[KEYWORD]
-        params[KEYWORD] = keyword
+    if search_keyword:
         if search_for == ALBUM:
-            filter_param = {'value__{}'.format(search_type): keyword}
-            params[RESULTS] = map(lambda x: {'column1': x.value},
-                                  Album.objects.filter(**filter_param).order_by('value'))
+            params[RESULTS] = _search_album(**params)
         elif search_for == SONG:
-            filter_param = {'title__{}'.format(search_type): keyword}
-            params[RESULTS] = map(lambda x: {'column1': x.title, 'column2': x.artist, 'column3': x.album},
-                                  Song.objects.filter(**filter_param).order_by('album__value', 'artist__value',
-                                                                               'title'))
-    except KeyError:
+            params[RESULTS] = _search_song(**params)
+    else:
         params[RESULTS] = []
-        params[KEYWORD] = ''
 
     return render(request, 'search.html', params)
