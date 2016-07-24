5b982d9905884ff4868002d7daaf2210b997c7aa
colajam93 <fs.fake.g1@gmail.com>
2016-07-24 12:44:23 +0900

    Convert es6 code with babel

--- common/static/js/main.es6.js
+++ common/static/js/main.es6.js
@@ -1,14 +1,12 @@
-'use strict';
-
 // https://docs.djangoproject.com/en/1.9/ref/csrf/#ajax
 function getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == name + '=') {
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
@@ -19,7 +17,6 @@ function getCookie(name) {
 
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method)
-    );
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
